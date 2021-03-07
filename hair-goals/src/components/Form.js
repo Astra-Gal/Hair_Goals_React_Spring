fdaadceeae27086dcf333b310e61f54d1f658654
Astra-Gal <giulia.galastro@gmail.com>
2021-03-07 17:52:20 +0000

    added form... not working yet

--- hair-goals/src/components/Form.js
+++ hair-goals/src/components/Form.js
@@ -1,10 +1,89 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-const Form = () => {
+const Form = ({onNewUserSubmit}) => {
+
+    const [name, setName] = useState("");
+
+    const [hairLength, setHairLength] = useState(0);
+
+    const [date, setDate] = useState("");
+
+    const [birthday, setBirthday] = useState("");
+
+    const handleNameChange = (evt) => {
+        setName(evt.target.value);
+    }
+
+    const handleHairLengthChange = (evt) => {
+        setHairLength(evt.target.value);
+    }
+
+    const handleDateChange = (evt) => {
+        setDate(evt.target.value);
+    }
+
+    const handleBirthdayChange = (evt) => {
+        setBirthday(evt.target.value);
+    }
+
+    const handleFormSubmit = (evt) => {
+        evt.preventDefault();
+        const nameToSubmit = name.trim();
+        const hairLengthToSubmit = hairLength.trim();
+        const dateToSubmit = date.trim(); // might need to reformat date
+        const birthdayToSubmit = birthday.trim();
+        if(!nameToSubmit || !hairLengthToSubmit || !dateToSubmit || birthdayToSubmit){
+            return
+        }
+
+        onNewUserSubmit({
+            name: nameToSubmit,
+            hairLength: hairLengthToSubmit,
+            date: dateToSubmit,
+            birthday: birthdayToSubmit
+            
+        });
+        
+
+        setName("");
+        setHairLength(0);
+        setDate("");
+        setBirthday("");
+        console.log("User has been submitted");
+    }
+
+
+
     return (
-        <>
-        <h2>I am FORM</h2>
-        </>
+        <form onSubmit={handleFormSubmit}>
+            <input
+            type="text"
+            placeholder="Your Name Please"
+            value={name}
+            onChange={handleNameChange}
+            />
+            <input
+            type="number"
+            placeholder="Your Hair Length Please"
+            value={hairLength}
+            onChange={handleHairLengthChange}
+            /> 
+            <input
+            type="text"
+            placeholder="What is Today"
+            value={date}
+            onChange={handleDateChange}
+            />
+            <input
+            type="text"
+            placeholder="Your Birthday Please"
+            value={birthday}
+            onChange={handleBirthdayChange}
+            />  
+            <input
+                type="Submit"
+            />                                   
+        </form>
     )
 }
 
--- hair-goals/src/containers/HairGoalsContainer.js
+++ hair-goals/src/containers/HairGoalsContainer.js
@@ -11,20 +11,38 @@ import ErrorPage from '../components/ErrorPage';
 
 
 const HairGoalsContainer = () => {
+
+    const [users, setUsers] = useState(
+        [
+            {
+                id:1,
+                name: "Mario",
+                hairLength: 60,
+                date:"02/04/2021",
+                birthday:"08/05/1991"
+            }
+        ]
+    )
+
+    const addNewUser = (newUser) => {
+        newUser.id = Date.now(); // eventually this id will come from db
+        const updatedUsers = [...users, newUser];
+        setUsers(updatedUsers);
+    }
+
     return (
         <Router>
             <>
-            <SiteHeader/>
-                <h1>This is a hair goals container</h1>
-                
+            <SiteHeader/>                
                 <Switch>
                     <Route exact path="/" component={Welcome}/>
-                    <Route path="/new-user" component={Form}/>
+                    <Route path="/new-user" render={() => <Form onNewUserSubmit={(user) => addNewUser(user)}/>}/>
                     <Route path="/user-details" component={UserData}/>
                     <Route path="/add-measurement" component={AddMeasurement}/>
                     <Route path="/edit-details" component={EditDetails}/>
                     <Route component={ErrorPage}/>
                 </Switch>
+                {/* <Form onNewUserSubmit={(user) => addNewUser(user)}/> */}
 
             </>
         </Router>
