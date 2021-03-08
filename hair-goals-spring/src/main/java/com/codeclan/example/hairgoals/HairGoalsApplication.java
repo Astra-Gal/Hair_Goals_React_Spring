6299be9b4a1ae01c1cde0c84433f5ee6fe366bc2
Astra-Gal <giulia.galastro@gmail.com>
2021-03-08 11:20:54 +0000

    spring sprung

---
+++ hair-goals-spring/src/main/java/com/codeclan/example/hairgoals/HairGoalsApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.hairgoals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HairGoalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HairGoalsApplication.class, args);
+	}
+
+}
