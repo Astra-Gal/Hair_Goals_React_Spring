41bdb651e6a7f4453823184c80822e1c7d8ece2c
Astra-Gal <giulia.galastro@gmail.com>
2021-03-10 10:46:12 +0000

    deleted users from dataloader

--- hair_goals_backend/src/main/java/com/codeclan/example/hair_goals_backend/DataLoader.java
+++ hair_goals_backend/src/main/java/com/codeclan/example/hair_goals_backend/DataLoader.java
@@ -21,33 +21,33 @@ public class DataLoader implements ApplicationRunner {
 
     }
     public void run (ApplicationArguments args){
-        User user1 = new User("Bob", 30, 50 );
-        user1.setTimeTillGoal();
-        userRepository.save(user1);
-
-        User user2 = new User("Bobbette", 40, 60 );
-        userRepository.save(user2);
-
-
-        Measurement measurement1 = new Measurement(30, "08/03/2021", user1);
-        measurementRepository.save(measurement1);
-
-        Measurement measurement2 = new Measurement(35, "09/03/2021", user1);
-        measurementRepository.save(measurement2);
-
-        Measurement measurement3 = new Measurement(42, "08/03/2021", user2);
-        measurementRepository.save(measurement3);
-
-        Measurement measurement4 = new Measurement(45, "09/03/2021", user2);
-        measurementRepository.save(measurement4);
-
-        user1.addMeasurement(measurement1);
-        user1.addMeasurement(measurement2);
-        userRepository.save(user1);
-
-        user2.addMeasurement(measurement3);
-        user2.addMeasurement(measurement4);
-        userRepository.save(user2);
+//        User user1 = new User("Bob", 30, 50 );
+//        user1.setTimeTillGoal();
+//        userRepository.save(user1);
+//
+//        User user2 = new User("Bobbette", 40, 60 );
+//        userRepository.save(user2);
+//
+//
+//        Measurement measurement1 = new Measurement(30, "08/03/2021", user1);
+//        measurementRepository.save(measurement1);
+//
+//        Measurement measurement2 = new Measurement(35, "09/03/2021", user1);
+//        measurementRepository.save(measurement2);
+//
+//        Measurement measurement3 = new Measurement(42, "08/03/2021", user2);
+//        measurementRepository.save(measurement3);
+//
+//        Measurement measurement4 = new Measurement(45, "09/03/2021", user2);
+//        measurementRepository.save(measurement4);
+//
+//        user1.addMeasurement(measurement1);
+//        user1.addMeasurement(measurement2);
+//        userRepository.save(user1);
+//
+//        user2.addMeasurement(measurement3);
+//        user2.addMeasurement(measurement4);
+//        userRepository.save(user2);
 
 
 
