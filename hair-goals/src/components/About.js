eda488e07772450201ac6e3064fc740bb55237cb
Astra-Gal <giulia.galastro@gmail.com>
2021-03-10 23:21:13 +0000

    added styling to Guide and About sections

--- hair-goals/src/components/About.js
+++ hair-goals/src/components/About.js
@@ -1,15 +1,22 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import NavBar from './NavBar';
+import Logo from './Logo';
 
 const About = () => {
     return (
         <>
-        <h1>Hair Goals</h1>
+        <NavBar/>
+        <Logo/>
         <h2>Keep track of your hair</h2>
         <p>
-            Have you shaved your head recently? Or maybe you just wana keep track of those Rapunzel locks.
-            Are you wondering how long its gona take you to reach all of those frustrating milestones? 
-            We are here for you! Track you hair length and we will keep you on your hair game. 
+            Have you shaved your head recently? Maybe you wanna keep track of those Rapunzel locks.
+        Are you wondering how long it's gonna take you to reach all of those frustrating milestones? 
+        We're here for you! Track you hair length and we will keep you on your hair game. 
         </p>
+        <div className="wrapper">
+        <Link to='/new-user' className="button-link"><button>Ready to start?<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M10.477 0h-8.977l12.024 12-12.024 12h8.977l12.023-12z"/></svg></button></Link>
+        </div>
         </>
     )
     }
--- hair-goals/src/components/Guide.js
+++ hair-goals/src/components/Guide.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import NavBar from './NavBar';
+import Logo from './Logo';
 
 const Guide = () => {
     return (
         <>
+        <NavBar/>
+        <Logo/>
         <h1>Measuring Guide</h1>
-        <h2>Wanna Know how to measure those lushious locks?</h2>
+        <h2>Wanna Know how to measure those luscious locks?</h2>
         <p>
             The length of your wavy hair texture lies between the curly and straight hair. 
             One of the accurate ways to measure your hair length is simply using a measuring tape from your forehead to the tip of your hair, where your hair length ends. 
