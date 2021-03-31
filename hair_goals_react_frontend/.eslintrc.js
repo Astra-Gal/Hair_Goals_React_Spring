7cf5c5f0f79eeda671027c313c0cc761f0abde21
Astra-Gal <giulia.galastro@gmail.com>
2021-03-31 22:30:16 +0100

    fixed eslint with alex'\s help

---
+++ hair_goals_react_frontend/.eslintrc.js
@@ -0,0 +1,38 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
+        commonjs: true,
+    },
+    extends: ['eslint:recommended', 'plugin:react/recommended'],
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 12,
+        sourceType: 'module',
+    },
+    plugins: ['react'],
+    rules: {
+        'react/jsx-uses-react': 'warn',
+        'react/jsx-uses-vars': 'warn',
+        'react/prop-types': ['warn'],
+        'no-restricted-imports': [
+            'warn',
+            {
+                patterns: [
+                    '@material-ui/*/*/*',
+                    '!@material-ui/core/test-utils/*',
+                ],
+            },
+        ],
+        'no-unused-vars': [
+            'warn',
+            {
+                varsIgnorePattern: 'React',
+            },
+        ],
+    },
+}
--- hair_goals_react_frontend/src/App.js
+++ hair_goals_react_frontend/src/App.js
@@ -2,18 +2,12 @@ import React from 'react';
 import HairGoalsContainer from './containers/HairGoalsContainer';
 import './App.css';
 
-
-
-
-
 function App() {
   return (
-    
     <div className="apphouse">
-      <HairGoalsContainer/>
+      <HairGoalsContainer />
     </div>
-    
-  )
+  );
 }
 
 export default App;
